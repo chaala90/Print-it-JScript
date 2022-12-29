6b46b14be33b1b02f02334c6c6f6c3f5d3b2d66a
dh ch <chaalaadhif@gmail.com>
2022-12-29 08:41:41 +0100

    modif finale

--- assets/script.js
+++ assets/script.js
@@ -13,59 +13,66 @@ let slides = [
   },
   {
     "image": "slide4.png",
-    "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"}];
+    "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+  }];
 // declarer un tableau
-let tab=[];
-tab=Object.values(slides);
-let banner=document.querySelector("#banner");
+let tab = [];
+//recuperer les objets d'un tableau
+tab = Object.values(slides);
+let banner = document.querySelector("#banner");
 //cibler la balise en passant par sa classe
-let slide=document.querySelector(".slide");
-let p=document.querySelector(".p");
+let slide = document.querySelector(".slide");
+let p = document.querySelector(".p");
 //recuperer les fleches gauches et droites
-let arrow_left=document.querySelector(".arrow_left");
+let arrow_left = document.querySelector(".arrow_left");
 arrow_left.addEventListener("click", previmg);
-let arrow_right=document.querySelector(".arrow_right");
+let arrow_right = document.querySelector(".arrow_right");
 arrow_right.addEventListener("click", nextimg);
-var i=0;
+//cilbler la balise qui contient la classe dot_selected
+let dot_img = document.getElementsByClassName("dot");
+var i = 0;
 //la longueur de tableau
 const numimg = tab.length;
 //CREATION DES POINTS DE SLIDER
-let dots=document.querySelector(".dots");
+let dots = document.querySelector(".dots");
 //savoir combien de point mettre
-for ( let i=0; i<=numimg-1; i++){
-  if (i=0) {
-    d.classList.add("dot_selected");
-  }
-let d=document.createElement("div");
-d.classList.add("dot");
-dots.appendChild(d);
+for (let i = 0; i <= numimg - 1; i++) {
+  //creer les elements div
+  let d = document.createElement("div");
+  //creer les classes dot
+  d.classList.add("dot");
+  //mettre les classes dot dans la classe dots
+  dots.appendChild(d);
 
 }
-
+//la fonction de gauche
 function previmg() {
-  // tab[0].image;
+  //retirer le dot_selected
+  dot_img[i].classList.remove("dot_selected");
   console.log(tab[0].image);
-
-  /*dot[i].classList.remove("dot_selected");*/
   i--;
   if (i < 0) {
     i = numimg - 1;
   }
+  //cibler le chemin des images 
   slide.src = "./assets/images/slideshow/" + tab[i].image;
-  p.innerHTML=tab[i].tagLine;
-  /* dot[i].classList.add("dot_selected");*/
-
+  //modifier le texte de l'image
+  p.innerHTML = tab[i].tagLine;
+  //remettre le dot-selected dans la position courante
+  dot_img[i].classList.add("dot_selected");
 }
 
 
+//la fonction de droite
 function nextimg() {
-  /*dot[i].classList.remove("dot_selected");*/
- i++;
-  if(i >numimg-1)
-  { i=0;
+  // retirer le dot-selected
+  dot_img[i].classList.remove("dot_selected");
+  i++;
+  if (i > numimg - 1) {
+    i = 0;
+  }
+  slide.src = "./assets/images/slideshow/" + tab[i].image;
+  p.innerHTML = tab[i].tagLine;
+  //remettre le dot-selected dans la position courante
+  dot_img[i].classList.add("dot_selected");
 }
-slide.src = "./assets/images/slideshow/" + tab[i].image;
-p.innerHTML=tab[i].tagLine;
- 
-   }
-   /*dot[i].classList.add("dot_selected");*/
